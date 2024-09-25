8cb173120b774efffd7b99480ad35b2719feac63
sumitra tamang <sumi8tamang@gmail.com>
2024-09-25 21:36:45 +0545

    feat: create Quantity btn

---
+++ components/QuantityButton.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const quantity =defineModel<number>()
+const IncreaseQty= ()=>{
+  if(quantity?.value){
+    quantity.value++
+  }
+}
+const DecreaseQty= ()=>{
+  if(quantity?.value && quantity.value>0){
+    quantity.value--
+  }
+}
+
+</script>
+<template>
+    <button class="font-bold " @click="DecreaseQty">-</button>
+    <input type="text" placeholder="1" v-model.number="quantity" class="h-10 w-10 m-2 text-center border-2 "/>
+    <button class="font-bold" @click="IncreaseQty">+</button>   
+</template>
