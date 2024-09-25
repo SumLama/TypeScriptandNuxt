081d4e24813966d730f48af5f01eb2c09799c9e4
sumitra tamang <sumi8tamang@gmail.com>
2024-09-25 21:39:56 +0545

    feat: add pinia persisted plugin to cartstore

--- composables/cartStores.ts
+++ composables/cartStores.ts
@@ -1,3 +1,7 @@
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
+import { createPinia } from 'pinia';
+export const pinia = createPinia();
+pinia.use(piniaPluginPersistedstate);
 interface cartItem{
     id:number,
     title:string,
@@ -5,28 +9,29 @@ interface cartItem{
     price:number,
     quantity:number  
 }
- export const cartStore = defineStore('cart',()=>{
-    const items =ref<Record<number,cartItem>>({})
-      
-    const itemCount = computed(()=>{ return Object.keys(items.value).length})
-    const totalPrice = computed(()=> {return Object.values(items.value).reduce((total:number,item)=>total+item.price*item.quantity,0)})
-    const addItem = (item:any)=>{
-        if(items.value[item.id]){
-            items.value[item.id].quantity += item.quantity
-        }
-        else{
-            items.value[item.id] = item
-        }
-    }
+export const cartStore = defineStore('cart', () => {
+    const items = ref<Record<number, cartItem>>({});
+
+    const itemCount = computed(() => Object.keys(items.value).length);
     
-    const removeItem =(itemId:number)=>{
-        if(items.value[itemId]){
-            delete items.value[itemId]
+    const totalPrice = computed(() => 
+        Object.values(items.value).reduce((total, item) => total + item.price * item.quantity, 0)
+    );
+
+    const addItem = (item: cartItem) => {
+        if (items.value[item.id]) {
+            items.value[item.id].quantity += item.quantity;
+        } else {
+            items.value[item.id] = item;
         }
-    }
+    };
 
-    return {items,itemCount,totalPrice,addItem,removeItem}
- })
-
-
-
+    const removeItem = (itemId: number) => {
+        if (items.value[itemId]) {
+            delete items.value[itemId];
+        }
+    };
+    return { items,itemCount,totalPrice,addItem,removeItem }
+}, {
+    persist: true, 
+});
