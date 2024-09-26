9a26517dbf5dfe1c9a07165bcd99527134546289
sumitra tamang <sumi8tamang@gmail.com>
2024-09-26 17:57:11 +0545

    feat: use type cartItem array for storing cart items,add increase/decrease qty method, clear method to clear store

--- composables/cartStores.ts
+++ composables/cartStores.ts
@@ -1,37 +1,56 @@
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
-import { createPinia } from 'pinia';
+import { createPinia, defineStore } from 'pinia';
+
 export const pinia = createPinia();
 pinia.use(piniaPluginPersistedstate);
-interface cartItem{
-    id:number,
-    title:string,
-    image:string,
-    price:number,
-    quantity:number  
+
+interface cartItem {
+    id: number;
+    title: string;
+    image: string;
+    price: number;
+    quantity: number;
 }
 export const cartStore = defineStore('cart', () => {
-    const items = ref<Record<number, cartItem>>({});
+    const items = ref<Array<cartItem>>([])
 
-    const itemCount = computed(() => Object.keys(items.value).length);
+    const itemCount = computed(() => items.value.length)
     
     const totalPrice = computed(() => 
-        Object.values(items.value).reduce((total, item) => total + item.price * item.quantity, 0)
+        items.value.reduce((total, item) => total + item.price * item.quantity, 0)
     );
 
     const addItem = (item: cartItem) => {
-        if (items.value[item.id]) {
-            items.value[item.id].quantity += item.quantity;
-        } else {
-            items.value[item.id] = item;
+        const existingItem = items.value.find(i => i.id === item.id)
+            if (existingItem) {
+                existingItem.quantity += item.quantity
+            } else {
+                items.value.push(item)
+            }  
+        }
+    const removeItem = (itemId: number) => {
+        items.value = items.value.filter(item => item.id !== itemId)
+    }
+
+    const increaseQty = (itemId: number) => {
+        const existingItem = items.value.find(i => i.id === itemId)
+        if (existingItem) {
+            existingItem.quantity++
         }
     };
 
-    const removeItem = (itemId: number) => {
-        if (items.value[itemId]) {
-            delete items.value[itemId];
+    const decreaseQty = (itemId: number) => {
+        const existingItem = items.value.find(i => i.id === itemId)
+        if (existingItem && existingItem.quantity > 1) {
+            existingItem.quantity--
         }
-    };
-    return { items,itemCount,totalPrice,addItem,removeItem }
-}, {
-    persist: true, 
-});
+    }
+    const clearStore = ()=>{
+        items.value =[]
+    }
+
+    return { items, itemCount, totalPrice, addItem, removeItem,increaseQty,decreaseQty,clearStore}
+}, 
+{
+    persist: true
+})
