94c23b9866ac6c0c1889389e7b562e92953ae06b
sumitra tamang <sumi8tamang@gmail.com>
2024-09-27 17:26:07 +0545

    feat: add route rules for redirection to prodcuts page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,10 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@nuxt/ui', '@pinia/nuxt','pinia-plugin-persistedstate/nuxt']
+  modules: ['@nuxt/ui', '@pinia/nuxt','pinia-plugin-persistedstate/nuxt'],
+  routeRules: {
+    "/": {
+      redirect: "/products"
+    }
+  }
 })
