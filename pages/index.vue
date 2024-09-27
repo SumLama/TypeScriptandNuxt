ee1acb491025b59bae3ed10f24afee975e1cc5ad
sumitra tamang <sumi8tamang@gmail.com>
2024-09-27 17:29:12 +0545

    fix: remove use of productList component

--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,2 @@
-<script setup lang="ts">
-interface Product{
-    id:number,
-    title:string,
-    image:string,
-    price:number,
-    description:string
-}
-
-const {data,status} = await useFetch<Product[]>("https://fakestoreapi.com/products")
-</script>
-
 <template>
-  
-    <div v-if="status==='pending'" class="text-center text-2xl">
-        <h1>Loading....</h1>
-    </div>
-    <div v-else-if="status==='success' && data">
-        <ProductList :products="data"/>
-    </div>
-   
 </template>
